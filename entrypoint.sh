a3b3f4058e3d9ea56cc38f61f2d53b560cc55650
Youngduk Suh <skyducks111@gmail.com>
2022-10-30 08:12:34 +0900

    Redirect output to variable

--- entrypoint.sh
+++ entrypoint.sh
@@ -37,4 +37,12 @@ if [ ! -f "$HOME/.kube/config" ]; then
     fi
 fi
 
-kubectl $*
+if [ -z "$dest" ]; then
+    kubectl $*
+else
+    output=$(kubectl $*)
+    echo "::set-output name=$dest::$output"
+
+    # debug only
+    #echo $output
+fi
